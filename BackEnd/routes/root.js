c6174c6d86fbf1bbab4162153d75040c54445525
ruzul <ruzulchaure2185@gmail.com>
2024-03-25 17:10:16 +0530

    d1

--- BackEnd/routes/root.js
+++ BackEnd/routes/root.js
@@ -3,8 +3,8 @@ const path = require('path');
 
 const router = express.Router();
 
-router.get('^/$|/index(.html)?',(req,res) => {
-    res.sendFile(path.join(__dirname,'..','views','index.html'))
+router.get('^/$|/login(.html)?',(req,res) => {
+    res.sendFile(path.join(__dirname,'..','views','login.html'))
 });
 
 module.exports = router;
