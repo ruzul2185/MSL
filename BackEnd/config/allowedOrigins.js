96f2cfe177162382552a7882c5aff2429175ed2d
ruzul <ruzulchaure2185@gmail.com>
2024-03-26 13:29:11 +0530

    Advice Added

--- BackEnd/config/allowedOrigins.js
+++ BackEnd/config/allowedOrigins.js
@@ -1,6 +1,7 @@
 const allowedOrigins = [
     'http://localhost:3000',
-    'http://localhost:3500'
+    'http://localhost:3500',
+    'https://msl-bcmh.onrender.com/',
 ];
 
 module.exports = allowedOrigins;
